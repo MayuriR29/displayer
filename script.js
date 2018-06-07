bcb87896452510bc7fb70f684980c046f8d84cc1
MayuriR29 <mayuri.gr@gmail.com>
2018-06-07 22:10:51 +0800

    Added changes

---
+++ script.js
@@ -0,0 +1,13 @@
+// function hello(){
+//     console.log('Hello');
+//     const name='Jumpstart';
+//     console.log('welcome to ',name);
+//     alert('Hello world');
+
+// }
+// hello();
+function getNumber(){
+    const inputValue=document.querySelector('#number').value; 
+    document.querySelector('#display').textContent=inputValue;
+}
+
